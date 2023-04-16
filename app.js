b135dd525b157c389e5e2824464c2cd3c6d1277a
Adi-Tri-17 <111167548+Adi-Tri-17@users.noreply.github.com>
2023-04-16 14:03:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => console.log('ServiceWorker registered'))
+        .catch(error => console.log('ServiceWorker registration failed: ', error));
+    });
+  }
+  
